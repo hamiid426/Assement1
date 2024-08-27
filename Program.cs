6cea9e328b41a47c573f947d13f889260f84403a
unknown <josu45537@gmail.com>
2024-08-26 17:05:09 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,184 @@
+﻿// Console.WriteLine("Enter a number");
+// int firstNumber = Convert.ToInt32(C30onsole.ReadLine());
+// if (firstNumber == 15)
+// {
+//    Console.WriteLine($"{firstNumber} is equal");
+// }
+// else 
+// {
+//    Console.WriteLine($"{firstNumber} is not equal");
+// }
+
+
+// Console.WriteLine("Enter a number");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num  % 2 == 0)
+// {
+// Console.WriteLine($"{num} is even");
+// }
+// else
+// {
+//    Console.WriteLine($"{num} is odd" );
+// }
+/*
+ Console.WriteLine("Enter a number");
+ int fig = Convert.ToInt32(Console. ReadLine());
+ if (fig % 2 == 0)
+ {
+ Console.WriteLine($"{fig} is positive ");
+ }
+ else
+ {
+ Console.WriteLine($"{fig} is negative ");
+}
+ Console.WriteLine("Enter a number");
+ int test = Convert.ToInt32(Console.ReadLine());
+ if (test == 21)
+ {
+    Console.WriteLine($"{test} Congratulation! you are eligible for casting your vote ");
+ }
+ else
+ {
+    Console.WriteLine($"{test} sorry! you are not eligible for casting your vote");
+ }
+Console.WriteLine("Enter your height in centimeter");
+int height = Convert.ToInt32(Console.ReadLine());
+if (height <= 135)
+{
+   Console.WriteLine($"{height} you are a dwarf");
+}
+else
+{
+   Console.WriteLine($"{height} you are not a dwarf");
+}
+
+// Console.WriteLine("Enter the value for m");
+// int m = Convert.ToInt32(Console.ReadLine());
+// if (m != 0)
+// {
+//  
+// {
+//    Console.WriteLine($"m is {m} when n = 1");
+// }
+// else if(m >= -5)
+// {
+//    Console.WriteLine($"m is {m} when n = 0");
+// }
+// else if(m <= -5)
+// {
+//  Console.WriteLine($"m is {m} when n = -1");
+// }
+// }
+// else
+// {
+//    Console.WriteLine("n = 0");
+// }
+
+int i = 5;
+while ( i>=0 )
+{
+   Console.WriteLine($"{i} continue the condition");
+i--;
+}   
+    
+int a = 10;
+while ( a>=0)
+{
+   Console.WriteLine($"{a} break");
+   a--;
+}
+
+in;;;;.;.;;..t c = 30;
+while ( c>=20)
+{
+   Console.WriteLine($"{c} open");
+   c--;
+}
+for (int c = 30; c>20; c++)
+{
+   Console.WriteLine($"{c} print");
+}
+
+for(int a = 40; a>20; a--)
+{
+   Console.WriteLine($"{a} Continue my condition");
+break;
+}
+
+int num = 100;
+while (num % 100 == 0 )
+{
+   Console.WriteLine($"{num} numbers between hundred are odd");
+   num--;
+}\
+Console.WriteLine ("enter your age");
+int age = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("input your name");
+string name = Console.ReadLine ();
+for (int n=30; n==30; n--) 
+{
+   Console.WriteLine($"{n} they are factorial number");
+break;
+} 
+//int n = 5;
+//while(n>=5);
+//{
+// Console.WriteLine($"{n} n = 1");
+//}
+Console.WriteLine ("enter factorial number");
+int factorial = Convert.ToInt32 (Console.ReadLine ());
+while (factorial*=5)
+{
+   Console.WriteLine ($"{factorial} break");
+}
+Console.WriteLine ("input your multiplicating number");
+int multiply = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine ("enter the the end of your multiplicating number");
+int end = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine ("multiplicating table of " + multiply +" to" + end);
+for (int m = 1; m <=end; m++) 
+{ 
+   Console.WriteLine (multiply +"*" + m +"="  + multiply*m );
+}
+for (int multiply = 1; multiply < 13; multiply++)
+{
+   for (int m=1; m < 13; m++)
+   {
+   Console.WriteLine(m +" * " +m+ "=" +multiply*m);
+   }
+}
+using System.Transactions;
+
+*/
+Console.WriteLine ("enter the sum of numbers");
+int sum =Convert.ToInt32 (Console.ReadLine ());
+  if (sum == 10)
+{
+    Console.WriteLine ($"{sum} is a positive integer ");
+}
+else
+{
+Console.WriteLine ($"{sum} is not a positive interger");
+}
+
+Console.WriteLine ("enter your input");
+int num =Convert.ToInt32 (Console.ReadLine());
+if  (num % 2 == 0)
+{
+   Console.WriteLine ($"{num} is even number");
+}
+else
+{
+   Console.WriteLine ($"{num} is odd number");
+}
+
+Console.WriteLine ("enter your largest number");
+int large =Convert.ToInt32 (Console.ReadLine());
+if (large >= 7)
+{
+   Console.WriteLine($"{large} is larger");
+   }
+   else
+   {
+      Console.WriteLine ($"{large} is lower");
+   }
